0a9cd3348205fe9030a74f0221d9c300273aa5d8
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-12-09 20:46:27 +0100

    day4and5

---
+++ Day4Task2/Program.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Finne en a på rad 2 og så kryss sjekke om det er m og s diagonalt begge veier
+ * 
+ * 
+ * 
+ */
+
+
+
+int total = 0;
+
+void ReadFile(string filepath)
+{
+    string line;
+    string[] lineArray = new string[3];
+    try
+    {
+        //Pass the file path and file name to the StreamReader constructor
+        StreamReader sr = new StreamReader(filepath);
+        //Read the first line of text
+        line = sr.ReadLine();
+        //Continue to read until you reach end of file
+            for (int i = 0; i < 3; i++)
+            {
+                lineArray[i] = line;
+                if (i < 2) line = sr.ReadLine();
+            }
+
+        while (line != null)
+        {
+            //Run code here
+            total += GetMas(lineArray);
+
+            //Read the next line
+            line = sr.ReadLine();     
+            lineArray[0] = lineArray[1];
+            lineArray[1] = lineArray[2];
+            lineArray[2] = line;
+            
+
+
+        }
+        //close the file
+        sr.Close();
+        Console.WriteLine(total);
+        Console.ReadLine();
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Executing finally block.");
+    }
+
+    return;
+}
+
+
+int GetMas(string[] lineArray){
+    int count = 0;
+
+    for (int i = 1; i < lineArray[1].Length - 1; i++) 
+    {
+        if (lineArray[1][i] == 'A')
+            if ((CheckDiagonalDownRight(lineArray, i) || CheckDiagonalUpLeft(lineArray, i)) && (CheckDiagonalDownLeft(lineArray, i) || CheckDiagonalUpRight(lineArray, i)))
+                count++;
+
+    }
+
+
+    return count;
+}
+
+
+
+bool CheckDiagonalDownLeft(string[] lineArray, int i)
+{
+    return (lineArray[2][i - 1] == 'S' && lineArray[0][i + 1] == 'M');
+}
+
+bool CheckDiagonalUpRight(string[] lineArray, int i)
+{
+    return (lineArray[2][i - 1] == 'M' && lineArray[0][i + 1] == 'S');
+}
+
+bool CheckDiagonalUpLeft(string[] lineArray, int i)
+{
+    return (lineArray[0][i - 1] == 'S' && lineArray[2][i + 1] == 'M');
+}
+
+
+bool CheckDiagonalDownRight(string[] lineArray, int i)
+{
+    return (lineArray[0][i - 1] == 'M' && lineArray[2][i + 1] == 'S');
+}
+
+ReadFile("C:\\Users\\robin\\source\\repos\\AdventOfCode\\Day4\\path.txt");
---
+++ Day5/Program.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * 1. Lese inn regler i et Map
+ * 2. Lese inn en og en linje fra updates - 
+ *    Vi leser inn en linje med split funksjon inn i en array av string - må gjøres om til array av int
+ *    Sjekke om de er gyldige - while: Hvis tallet er det første i mapet så skal vi feile hvis det andre tallet er foran på linja, hvis tallet er andre i mapet skal vi feile hvis det første tallet er etter på linja
+ *    Hvis gyldig, ta vare på tallet i midten
+ * 
+ * 
+ * 
+ * 
+ */
+
+List<KeyValuePair<int, int>> rulePairs = new List<KeyValuePair<int, int>>();
+int total = 0;
+void ReadFileRules(string filepath)
+{
+    string line;
+    try
+    {
+        //Pass the file path and file name to the StreamReader constructor
+        StreamReader sr = new StreamReader(filepath);
+        //Read the first line of text
+        line = sr.ReadLine();
+        //Continue to read until you reach end of file
+        while (line != null)
+        {
+            //write the line to console window
+            //Read line, and split it by whitespace into an array of strings
+            int[] pageNumbers = Array.ConvertAll(line.Split('|'), int.Parse);
+            rulePairs.Add(new KeyValuePair<int, int>(pageNumbers[0], pageNumbers[1]));
+
+            //Read the next line
+            line = sr.ReadLine();
+        }
+        //close the file
+        sr.Close();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Executing finally block.");
+    }
+}
+
+void ReadFileUpdates(string filepath)
+{
+    string line;
+    try
+    {
+        //Pass the file path and file name to the StreamReader constructor
+        StreamReader sr = new StreamReader(filepath);
+        //Read the first line of text
+        line = sr.ReadLine();
+        //Continue to read until you reach end of file
+        while (line != null)
+        {
+            //write the line to console window
+            //Read line, and split it by whitespace into an array of strings
+ 
+            var pageNumberArray = line.Split(',');
+            int[] pageNumbers = Array.ConvertAll(pageNumberArray, int.Parse);
+
+            for (int i = 0; i < pageNumbers.Length; i++) 
+            {
+                foreach (var rulePair in rulePairs) 
+                {
+                    if (pageNumbers[i] == rulePair.Key)
+                    {
+                        if (!CheckIfKeyValid(pageNumbers, rulePair.Value, i))
+                            goto End;
+                    }
+                    else if (pageNumbers[i] == rulePair.Value)
+                    {
+                        if (!CheckIfValueIsValid(pageNumbers, rulePair.Key, i))
+                            goto End;
+                    }
+
+                }
+            }
+            //Finn miderste verdi.
+            total += pageNumbers[(int)Math.Ceiling(((decimal)pageNumbers.Length / 2) - 1)];
+End:
+
+            //Read the next line
+            line = sr.ReadLine();
+        }
+        //close the file
+        sr.Close();
+        Console.WriteLine(total);
+        Console.ReadLine();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Executing finally block.");
+    }
+}
+
+bool CheckIfValueIsValid(int[] pageNumbers, int key, int index)
+{
+    if (index == pageNumbers.Length - 1) return true;
+    for (int i = index + 1; i < pageNumbers.Length; i++)
+        if (pageNumbers[i] == key)
+        { return false; }
+    return true;
+}
+
+bool CheckIfKeyValid(int[] pageNumbers, int value, int index)
+{
+
+    //   
+    //   65|51
+    //KeyPair
+    //51,64,65,16,71
+    for (int i = 0; i < index; i++)
+        if (pageNumbers[i] == value)
+            { return false; }
+    return true;
+}
+
+ReadFileRules("C:\\Users\\robin\\source\\repos\\AdventOfCode\\Day5\\Rules.txt");
+ReadFileUpdates("C:\\Users\\robin\\source\\repos\\AdventOfCode\\Day5\\Updates.txt");
+
+
