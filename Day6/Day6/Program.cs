70c6902952359ef10b42f27ed43310590a11b5c5
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-12-26 14:23:46 +0100

    x

--- Day6/Day6/Program.cs
+++ Day6/Day6/Program.cs
@@ -23,6 +23,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 string direction = "north";
 int ZeroHit = 0;
 int infinityLoops = 0;
+string lastZeroDirection = "north";
 
 void ReadFile(string filepath, bool part2)
 {
@@ -31,8 +32,11 @@ void ReadFile(string filepath, bool part2)
     int i = 0;
     List<string> rowsList = new List<string>();
     int[] currentPos = new int[2];
+    int[] startPos = new int[2];
     int[] lastZeroPos = new int[2];
-    string lastZeroDirection = "north";
+    int numberOfWalks = 0;
+    List<string> earlierZeroes = new List<string>();
+    
     try
     {
         //Pass the file path and file name to the StreamReader constructor
@@ -57,32 +61,165 @@ void ReadFile(string filepath, bool part2)
         FindStart(rowsList, currentPos);
         lastZeroPos[0] = currentPos[0];
         lastZeroPos[1] = currentPos[1];
-        AddZero(rowsList, lastZeroPos, lastZeroDirection);
-        while (currentPos[0] != -1)
+        startPos[0] = currentPos[0];
+        startPos[1] = currentPos[1];
+        if (part2)
+        {
+            AddZero(rowsList, lastZeroPos, lastZeroDirection);
+            while (lastZeroPos[0] != -1)
+            {
+                Console.WriteLine(lastZeroPos[0]);
+                Console.WriteLine(lastZeroPos[1]);
+
+                currentPos[0] = startPos[0];
+                currentPos[1] = startPos[1];
+                direction = "north";
+                numberOfWalks = 0;
+                
+                while (currentPos[0] != -1)
+                {
+                    
+                    numberOfWalks++;
+                    Walk(rowsList, currentPos);
+                    if (numberOfWalks == 10000)
+                    {
+                        string lastZeroPosString = ("00" + lastZeroPos[0].ToString()) + ("00" + lastZeroPos[1].ToString());
+                        if (earlierZeroes.Contains(lastZeroPosString))
+                            break;
+                        infinityLoops++;
+                        earlierZeroes.Add(lastZeroPosString);
+                        break;
+                    }
+                }
+                ZeroHit = 0;
+                RemoveZero(rowsList, lastZeroPos);
+                AddZero(rowsList, lastZeroPos, lastZeroDirection);
+            }
+            
+           
+        }
+        else    
+            while (currentPos[0] != -1)
         {
             Walk(rowsList, currentPos);
-            if (ZeroHit == 2)
-            {
-                ZeroHit = 0;
-                infinityLoops++;
-                break;
-            }
                 
         }
-            
+        
         total = CountXes(rowsList);
-        Console.WriteLine("Total i part 1 er: " + total);
-        Console.WriteLine("Infinity loops er: " + infinityLoops);
-        Console.ReadLine();
+        if(!part2)
+            Console.WriteLine("Total i part 1 er: " + total);
+        else
+        {
+            Console.WriteLine("Infinity loops er: " + infinityLoops);
+            Console.ReadLine();
+        }
+           
     }
     catch (Exception e)
     {
         Console.WriteLine("Exception: " + e.Message);
     }
-    finally
+}
+
+void RemoveZero(List<string> rowsList, int[] lastZeroPos)
+{
+    Console.WriteLine("Remove 0");
+    AddCharacter(rowsList, lastZeroPos, '.');
+
+}
+
+void ZeroWalkNorth(List<string> rowsList, int[] lastZeroPos)
+{
+    Console.WriteLine("Checking North");
+    var nextNorthChar = rowsList[lastZeroPos[0] - 1][lastZeroPos[1]];
+    Console.WriteLine(nextNorthChar);
+    if (nextNorthChar == '#' || nextNorthChar == '0')
     {
-        Console.WriteLine("Executing finally block.");
+        lastZeroDirection = "east";
+        ZeroWalkEast(rowsList, lastZeroPos);
+        return;
     }
+        
+    lastZeroPos[0] = lastZeroPos[0] - 1;
+    if (lastZeroPos[0] == -1)
+        lastZeroPos[1] = -1;
+    else
+        AddCharacter(rowsList, lastZeroPos, '0');
+}
+
+void ZeroWalkEast(List<string> rowsList, int[] lastZeroPos)
+{
+    Console.WriteLine("Checking East");
+    var nextEastChar = rowsList[lastZeroPos[0]][lastZeroPos[1] + 1];
+    Console.WriteLine(nextEastChar);
+    if (nextEastChar == '#' || nextEastChar == '0')
+    {
+        lastZeroDirection = "south";
+        ZeroWalkSouth(rowsList, lastZeroPos);
+        return;
+    }
+        
+    lastZeroPos[1] = lastZeroPos[1] + 1;
+    if (lastZeroPos[1] > rowsList[lastZeroPos[0]].Length - 1)
+    {
+        lastZeroPos[0] = -1;
+        lastZeroPos[1] = -1;
+    }
+    else
+        AddCharacter(rowsList, lastZeroPos, '0');
+}
+
+void ZeroWalkSouth(List<string> rowsList, int[] lastZeroPos)
+{
+    Console.WriteLine("Checking South");
+    if (lastZeroPos[0] < rowsList.Count - 1)
+    {
+
+        var nextSouthChar = rowsList[lastZeroPos[0] + 1][lastZeroPos[1]];
+        Console.WriteLine(nextSouthChar);
+        if (nextSouthChar == '#' || nextSouthChar == '0')
+        {
+            lastZeroDirection = "west";
+            ZeroWalkWest(rowsList, lastZeroPos);
+            return;
+        }
+    }
+        
+    lastZeroPos[0] = lastZeroPos[0] + 1;
+    Console.WriteLine("Last zero pos er: " + lastZeroPos[0]);
+    Console.WriteLine("RowslistCount -1 er" + (rowsList.Count - 1));
+    if (lastZeroPos[0] > rowsList.Count - 1)
+    {
+        lastZeroPos[0] = -1;
+        lastZeroPos[1] = -1;
+        return;
+    }
+    AddCharacter(rowsList, lastZeroPos, '0');
+}
+
+void ZeroWalkWest(List<string> rowsList, int[] lastZeroPos)
+{
+    Console.WriteLine("Checking West");
+    if (lastZeroPos[1] > 0)
+    {
+        var nextWestChar = rowsList[lastZeroPos[0]][lastZeroPos[1] - 1];
+        Console.WriteLine(nextWestChar);
+        if (nextWestChar == '#' || nextWestChar == '0')
+        {
+            lastZeroDirection = "north";
+            ZeroWalkNorth(rowsList, lastZeroPos);
+            return;
+        }
+
+
+    }
+    lastZeroPos[1] = lastZeroPos[1] - 1;
+    if (lastZeroPos[1] == -1)
+        lastZeroPos[0] = -1;
+
+    else
+    AddCharacter(rowsList, lastZeroPos, '0');
+
 }
 
 void AddZero(List<string> rowsList, int[] lastZeroPos, string lastZeroDirection)
@@ -91,20 +228,16 @@ void AddZero(List<string> rowsList, int[] lastZeroPos, string lastZeroDirection)
     {
         
         case "north":
-            lastZeroPos[0]--;
-            AddCharacter(rowsList, lastZeroPos, '0'); 
+            ZeroWalkNorth(rowsList, lastZeroPos);
             break;
         case "east":
-            lastZeroPos[1]++;
-            AddCharacter(rowsList, lastZeroPos, '0');
+            ZeroWalkEast(rowsList, lastZeroPos);
             break;
         case "south":
-            lastZeroPos[0]++;
-            AddCharacter(rowsList, lastZeroPos, '0');
+            ZeroWalkSouth(rowsList, lastZeroPos);
             break;
         case "west":
-            lastZeroPos[1]--;
-            AddCharacter(rowsList, lastZeroPos, '0');
+            ZeroWalkWest(rowsList, lastZeroPos);
             break;
     }
 }
@@ -219,9 +352,6 @@ void WalkSouth(List<string> rowsList, int[] currentPos)
     }
 
 }
-
-//..
-//..
 
 void WalkWest(List<string> rowsList, int[] currentPos)
 {
